f70348d6868dd99b69f46ec24c43215f470e22e0
JWX <jwx@github.com>
2021-12-30 18:34:24 +0800

    modify article api

--- go.mod
+++ go.mod
@@ -3,10 +3,13 @@ module gin-blog
 go 1.16
 
 require (
+	github.com/OwnLocal/goes v1.0.0 // indirect
+	github.com/astaxie/beego v1.12.3 // indirect
 	github.com/gin-gonic/gin v1.7.7
 	github.com/go-ini/ini v1.66.2 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/jinzhu/gorm v1.9.16 // indirect
-	github.com/mattn/go-sqlite3 v2.0.1+incompatible // indirect
+	github.com/siddontang/go v0.0.0-20180604090527-bdc77568d726 // indirect
+	github.com/siddontang/ledisdb v0.0.0-20181029004158-becf5f38d373 // indirect
 	github.com/unknwon/com v1.0.1 // indirect
 )
--- routers/v1/article.go
+++ routers/v1/article.go
@@ -1,9 +1,40 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"gin-blog/models"
+	"gin-blog/pkg/e"
+	"log"
+	"net/http"
+
+	"github.com/astaxie/beego/validation"
+	"github.com/gin-gonic/gin"
+	"github.com/unknwon/com"
+)
 
 //获取单个文章
 func GetArticle(c *gin.Context) {
+	id := com.StrTo(c.Param("id")).MustInt()
+	valid := validation.Validation{}
+	valid.Min(id, 1, "id").Message("ID必须大于0")
+	code := e.INVALID_PARAMS
+	var data interface{}
+	if !valid.HasErrors() {
+		if models.ExistArticleByID(id) {
+			data = models.GetArticle(id)
+			code = e.SUCCESS
+		} else {
+			code = e.ERROR_NOT_EXIST_ARTICLE
+		}
+	} else {
+		for _, err := range valid.Errors {
+			log.Printf("err.key: %s, err.message: %s", err.Key, err.Message)
+		}
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg":  e.GetMsg(code),
+		"data": data,
+	})
 }
 
 //获取多个文章
