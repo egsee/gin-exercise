9ea86c4aeb67b7374654e727ccc54243718d815c
JWX <jwx@github.com>
2022-01-12 15:58:13 +0800

    crud

--- models/article.go
+++ models/article.go
@@ -54,7 +54,19 @@ func GetArticle(id int) (article Article) {
 	return
 }
 
-func UpdateArticle(id int, data interface{}) bool {
+func EditArticle(id int, data interface{}) bool {
 	db.Model(&Article{}).Where("id = ?", id).Updates(data)
+	return true
+}
+
+func AddArticle(data map[string]interface{}) bool {
+	db.Create(&Article{
+		TagID:     data["tag_id"].(int),
+		Title:     data["title"].(string),
+		Desc:      data["desc"].(string),
+		Content:   data["content"].(string),
+		CreatedBy: data["created_by"].(string),
+		State:     data["state"].(int),
+	})
 	return true
 }
